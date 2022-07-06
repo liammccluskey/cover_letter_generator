a58abc16f512740ad2f22ad4b80bb8b2dab23615
Liam Mccluskey <liammail100@gmail.com>
2022-07-06 15:32:20 -0500

    first commit

---
+++ main.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Inputs
+# 1 : JOB TITLE
+# 2 : COMPNANY NAME
+
+jobTitle=$1
+companyName=$2
+
+# run python script with arguments listed above
+# output pdf of custom cover letter to the directory ./output
---
+++ readme.txt
@@ -0,0 +1,9 @@
+# Bash Script for Generator custom cover letter PDF
+
+# TO RUN
+
+bash main.sh [job title] [company name]
+
+# OUTPUT
+
+PDF of custom cover letter
